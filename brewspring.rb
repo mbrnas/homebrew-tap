04f8cf52909bfcb4b816131822eb5c14bc7a83e5
Matija Brnas <123379999+mbrnas@users.noreply.github.com>
2023-12-07 18:36:09 +0100

    Update brewspring.rb

--- brewspring.rb
+++ brewspring.rb
@@ -2,7 +2,7 @@ class Brewspring < Formula
     desc "Spring Boot project initializer script"
     homepage "https://github.com/mbrnas/brew-spring"
   
-    url "https://github.com/yourusername/yourrepository/archive/v1.0.0.tar.gz"
+    url "https://github.com/mbrnas/homebrew-tap/archive/v1.0.0.tar.gz"
     sha256 "83b7c875fe29ac558f1b15449e47f34192ae4163bebea9811e950bdfdf12f12a"
 
   
