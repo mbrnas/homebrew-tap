2dbc79f4e9681b059d5217e1951acd3cf99bac16
Matija Brnas <mb8828@rit.edu>
2023-12-07 21:16:28 +0100

    default installation

--- brewspring.rb
+++ brewspring.rb
@@ -1,17 +1,15 @@
 class Brewspring < Formula
-    desc "Spring Boot project initializer script"
-    homepage "https://github.com/mbrnas/brew-spring"
-  
-    url "https://github.com/mbrnas/brew-spring/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "83b7c875fe29ac558f1b15449e47f34192ae4163bebea9811e950bdfdf12f12a"
+  desc "Spring Boot project initializer script"
+  homepage "https://github.com/mbrnas/brew-spring"
 
-  
-    def install
-      bin.install "brewspring.sh"
-    end
-  
-    test do
-      system "#{bin}/brewspring.sh", "--version"
-    end
+  url "https://github.com/mbrnas/brew-spring/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "83b7c875fe29ac558f1b15449e47f34192ae4163bebea9811e950bdfdf12f12a"
+
+  def install
+    bin.install "brewspring.sh" => "brewspring"
   end
-  
+
+  test do
+    system "#{bin}/brewspring", "--version"
+  end
+end
